9aaf869562212e62a186d9dbf025a2594162169c
icoo08217 <icoo08216@gmail.com>
2022-09-27 11:15:38 +0900

    Product 샘플 데이터 2개 생성

--- src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData {
     @Bean
-    public CommandLineRunner initData(MemberService memberService){
+    public CommandLineRunner initData(MemberService memberService , ProductService productService){
         return args ->
         {
             String password = "{noop}1234";
@@ -19,6 +19,9 @@ public class DevInitData {
             Member member2 = memberService.join("user2", password, "user2@test.com");
             Member member3 = memberService.join("user3", password, "user3@test.com");
             Member member4 = memberService.join("user4", password, "user4@test.com");
+
+            productService.create("단가라 OPS" , 68000 , "청평화 A-1-15");
+            productService.create("쉬폰 OPS" , 72000 , "청평화 A-1-15");
         };
     }
 }
--- src/main/java/com/ll/exam/app_2022_09_22/app/member/repository/MemberRepository.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/member/repository/MemberRepository.java
@@ -2,8 +2,6 @@ package com.ll.exam.app_2022_09_22.app.member.repository;
 
 import com.ll.exam.app_2022_09_22.app.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
